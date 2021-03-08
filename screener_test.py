4e89804279cbe8fb2e9ba2809863403f8aff7173
XCram <45154364+XCram@users.noreply.github.com>
2021-03-08 05:37:46 +0300

    Update screener_test.py

--- screener_test.py
+++ screener_test.py
@@ -1,9 +1,14 @@
 import logging
 import requests
 import datetime
+import time
 import mysql.connector
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO) #, filename='screener_test.log'
+
+#INPUT_PAIRS_TUPLE = ('DOGEUSDT', 'COTIUSDT', 'PERLUSDT', 'AUCTIONBTC', 'TROYUSDT', 'REEFUSDT')
+INPUT_PAIRS_TUPLE = ('DOGEUSDT', 'COTIUSDT')
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO) #, filename='new_screener_test.log'
 logger = logging.getLogger(__name__)
 
 #####################DATABASE SCHEME####################
@@ -15,7 +20,7 @@ MySQL_TABLES['screener'] = (
     "  `id` int(11) NOT NULL AUTO_INCREMENT,"
     "  `date` datetime NOT NULL,"
     "  `pair` varchar(30),"
-    "  `time_range` varchar(30),"
+    "  `time_range` varchar(36),"
     "  `min_prcnt` DECIMAL(3,1) NOT NULL DEFAULT 0.0,"
     "  `common_prcnt` DECIMAL(3,1) NOT NULL DEFAULT 0.0,"
     "  `num_cycles` int(12) NOT NULL,"
@@ -89,21 +94,22 @@ class ScreenerDB:
         logger.error('MySQL database connection error: %s', e)
 
 class ScreenerBinance:
-    screeners_total = 0
+    weight_limit = 600 #Max 1200 weight per 1 minute
+    res_weight = 0
     DATE_FORMAT = '%Y-%m-%d %H:%M'
-    def __init__(self, pair, period, min_prcnt):
-      ScreenerBinance.screeners_total += 1
-      self.pair = pair
+    OUTPUT_DATE_FORMAT = '%d-%m-%Y %H:%M'
+    def __init__(self, pairs, period, min_prcnt):
+      self.pair_tuple = pairs
       self.period = period
       self.min_prcnt = min_prcnt
 
-    def get_pair_OHLC_list(self, **add_data):
+    def get_pair_OHLC_list(self, pair, **add_data):
       open_list = []
       highs_list = []
       lows_list = []
       close_list = []
       data_valid = False
-      q_data = {'symbol': self.pair, 'interval': self.period} #'limit': num_candles
+      q_data = {'symbol': pair, 'interval': '5m'} #'limit': num_candles
       if 'date_from' in add_data:
         start_utime = datetime.datetime.strptime(add_data['date_from'], ScreenerBinance.DATE_FORMAT).timestamp()
         start_time_msec = int(start_utime * 1000)
@@ -122,10 +128,15 @@ class ScreenerBinance:
       return data_valid, open_list, highs_list, lows_list, close_list
 
     def common_public_GET_request(self, req_url, **add_data):
+      if ScreenerBinance.res_weight > ScreenerBinance.weight_limit:
+        time.sleep(1)
       success = False
       try:
         response = requests.get(req_url, add_data)
         #print(response.url)
+        #print(response.headers)
+        if 'x-mbx-used-weight' in response.headers:
+          ScreenerBinance.res_weight = int(response.headers['x-mbx-used-weight'])
         if response.status_code == requests.codes.ok:
           resp_result = response.json()
           #print(list(resp_result.keys()))
@@ -144,46 +155,95 @@ class ScreenerBinance:
       multiplier = 10 ** decimals
       return int(n * multiplier) / multiplier
 
-    def calculate(self):
-      #date_to = datetime.datetime.today().strftime(self.DATE_FORMAT)
-      date_from = (datetime.datetime.today() - datetime.timedelta(hours=24)).strftime(self.DATE_FORMAT)
-      ohlc_lists = self.get_pair_OHLC_list(date_from=date_from)
-      #print(len(ohlc_lists[2]), len(ohlc_lists[3]))
-      highs_gen = (x for x in ohlc_lists[2])
-      lows_gen = (y for y in ohlc_lists[3])
-      last_price = ohlc_lists[1][0]
+    def process_main_algo(self, open_price, highs_list, lows_list):
+      highs_gen = (x for x in highs_list)
+      lows_gen = (y for y in lows_list)
+      last_price = open_price
+      last_trend = ''
+      prev_dev_prcnt = None
       cycles_list = []
-      output_data = []
+      uptrend_list = []
+      downtrend_list = []
       while last_price:
         try:
           next_high = next(highs_gen)
           next_low = next(lows_gen)
-          last_deviation_high_prcnt = (100*next_high/last_price) - 100
-          last_deviation_low_prcnt = 100 - (100*next_high/last_price)
-          if last_deviation_high_prcnt > self.min_prcnt:
+          next_high_index = highs_list.index(next_high)
+          next_low_index = lows_list.index(next_low)
+          prev_high = highs_list[next_high_index-1]
+          prev_low = lows_list[next_low_index-1]
+          if not last_trend:
+            last_deviation_high_prcnt = (100*next_low/last_price) - 100
+            last_deviation_low_prcnt = 100 - (100*next_high/last_price)
+            #last_deviation_prcnt = 100 - (100*next_high/last_price)
+            if last_deviation_high_prcnt > self.min_prcnt:
+              last_price = next_low
+              last_trend = 'up'
+              prev_dev_prcnt = last_deviation_high_prcnt
+            elif last_deviation_low_prcnt > self.min_prcnt:
+              last_price = next_high
+              last_trend = 'down'
+              prev_dev_prcnt = last_deviation_low_prcnt
+              #next_prcnt = -1 * last_deviation_low_prcnt
+          elif last_trend == 'up':
             last_price = next_high
-            cycles_list.append(last_deviation_high_prcnt)
-          elif last_deviation_low_prcnt > self.min_prcnt:
+            last_deviation_prcnt = (100*next_high/prev_low) - 100
+            price_run = last_deviation_prcnt - prev_dev_prcnt
+            if price_run > self.min_prcnt:
+              cycles_list.append(last_deviation_prcnt)
+              downtrend_list.append(last_deviation_prcnt)
+              last_trend = 'down'
+            else:
+              prev_dev_prcnt = last_deviation_prcnt
+          elif last_trend == 'down':
             last_price = next_low
-            next_prcnt = -1 * last_deviation_low_prcnt
-            cycles_list.append(next_prcnt)
+            last_deviation_prcnt = 100 - (100*next_low/prev_high)
+            price_run = last_deviation_prcnt - prev_dev_prcnt
+            if price_run > self.min_prcnt:
+              cycles_list.append(last_deviation_prcnt)
+              uptrend_list.append(last_deviation_prcnt)
+              last_trend = 'up'
+            else:
+              prev_dev_prcnt = last_deviation_prcnt
         except:
           last_price = None
-      min_prcnt = self.truncate(min(cycles_list), 1)
       common_prcnt = sum(cycles_list)
       cycles_len = len(cycles_list)
-      avg_common_prcnt = self.truncate(common_prcnt / cycles_len, 1)
-      up_list = [up for up in cycles_list if up > 0]
-      down_list = [down for down in cycles_list if down < 0]
-      avg_up = self.truncate(sum(up_list) / len(up_list), 1)
-      avg_down = self.truncate(sum(down_list) / len(down_list), 1)
-      output_data = (datetime.datetime.now(), self.pair, 'Last 24 hours', min_prcnt,
-        common_prcnt, cycles_len, avg_common_prcnt, avg_up, avg_down)
+      avg_common_prcnt = 0.0
+      avg_up = 0.0
+      avg_down = 0.0
+      if cycles_len > 0:
+        avg_common_prcnt = self.truncate(common_prcnt / cycles_len, 1)
+      if len(uptrend_list) > 0:
+        avg_up = self.truncate(sum(uptrend_list) / len(uptrend_list), 1)
+      if len(downtrend_list) > 0:
+        avg_down = self.truncate(sum(downtrend_list) / len(downtrend_list), 1)
+      output_date_to = datetime.datetime.now().strftime(ScreenerBinance.OUTPUT_DATE_FORMAT)
+      output_date_from = (datetime.datetime.now() - datetime.timedelta(hours=self.period)).strftime(ScreenerBinance.OUTPUT_DATE_FORMAT)
+      out_list = [datetime.datetime.now(), f'{output_date_from} - {output_date_to}', self.min_prcnt,
+        common_prcnt, cycles_len, avg_common_prcnt, avg_up, avg_down]
+      return out_list
+
+    def start(self):
+      #date_to = datetime.datetime.today().strftime(self.DATE_FORMAT)
+      output_data = []
+      date_from = (datetime.datetime.today() - datetime.timedelta(hours=self.period)).strftime(self.DATE_FORMAT)
+      for pair in self.pair_tuple:
+        ohlc_lists = self.get_pair_OHLC_list(pair, date_from=date_from)
+        res_list = self.process_main_algo(ohlc_lists[1][0], ohlc_lists[2], ohlc_lists[3])
+        res_list.insert(1, pair)
+        res_tuple = tuple(res_list)
+        output_data.append(res_tuple)
       return output_data
 
 if __name__ == '__main__':
   scr_db = ScreenerDB(MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DATABASE)
   scr_db.MySQL_create_tables(MySQL_TABLES)
-  screener = ScreenerBinance('DOGEUSDT', '5m', 1)
-  res = screener.calculate()
-  scr_db.MySQL_add_new_row_to_table('screener', MYSQL_COLUMNS, res)
+  start_time = time.time()
+  screener = ScreenerBinance(INPUT_PAIRS_TUPLE, 24, 1)
+  res = screener.start()
+  scr_db.MySQL_add_many_rows_to_table('screener', MYSQL_COLUMNS, res)
+  end_time = time.time()
+  total_time = end_time - start_time
+  logger.info('Time total: %f', total_time)
+
